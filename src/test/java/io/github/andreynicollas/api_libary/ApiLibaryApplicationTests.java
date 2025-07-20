ed546e7fef29855d8dbb2a9f07b2bf50178c69e0
Andrey Nicollas <adsandr3y24@gmail.com>
2025-07-20 16:11:50 -0300

    Projeto criado

---
+++ src/test/java/io/github/andreynicollas/api_libary/ApiLibaryApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.andreynicollas.api_libary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLibaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
