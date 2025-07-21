1d150fb7275de2d42ffd01e8c22cde092dfe51b7
Andrey Nicollas <adsandr3y24@gmail.com>
2025-07-21 12:15:36 -0300

    Trabalhando com relacionamentos entre as entidades Livro e Autor

---
+++ src/test/java/io/github/andreynicollas/api_libary/repository/LivroRepositoryTest.java
@@ -0,0 +1,39 @@
+package io.github.andreynicollas.api_libary.repository;
+
+import io.github.andreynicollas.api_libary.model.Autor;
+import io.github.andreynicollas.api_libary.model.GeneroLivro;
+import io.github.andreynicollas.api_libary.model.Livro;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@SpringBootTest
+class LivroRepositoryTest {
+
+    @Autowired
+    LivroRepository repository;
+
+    @Autowired
+    AutorRepository autorRepository;
+
+    @Test
+    void salvarTest() {
+        Livro livro = new Livro();
+        livro.setIsbn("12345-67890");
+        livro.setPreco(BigDecimal.valueOf(100));
+        livro.setGenero(GeneroLivro.FICCAO);
+        livro.setTitulo("UFO");
+        livro.setDataPublicacao(LocalDate.of(1980,1,2));
+
+        Autor autor = autorRepository
+                .findById(UUID.fromString("e53c75c2-4393-4587-8da7-b27ff139839b"))
+                .orElse(null);
+
+        livro.setAutor(autor);
+        repository.save(livro);
+    }
+}
