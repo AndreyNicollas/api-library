8d1bcb030c56ce3154c77fdfe113d11c482fdd55
Andrey Nicollas <adsandr3y24@gmail.com>
2025-07-21 10:28:46 -0300

    Mapeamento das entidades autor e livro com JPA e utilização do Lombok

---
+++ src/main/java/io/github/andreynicollas/api_libary/model/Livro.java
@@ -0,0 +1,40 @@
+package io.github.andreynicollas.api_libary.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "livro")
+@Getter
+@Setter
+public class Livro {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "isbn", length = 20, nullable = false)
+    private String isbn;
+
+    @Column(name = "titulo", length = 150, nullable = false)
+    private String titulo;
+
+    @Column(name = "data_publicacao")
+    private LocalDate dataPublicacao;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "genero", length = 50, nullable = false)
+    private GeneroLivro genero;
+
+    @Column(name = "preco", precision = 18, scale = 2)
+    private Double preco;
+
+    @ManyToOne
+    @JoinColumn(name = "id_autor")
+    private Autor autor;
+}
